1da2730c754a196d370b56f4acf07dc614caad18
hun5077@gmail.com <hun5077@gmail.com>
2018-12-28 23:28:41 +0900

    thirdparty 빌드 스크립트 업데이트

--- thirdparty/thirdparty_build.sh
+++ thirdparty/thirdparty_build.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 root_path=$(pwd)
-=======
-pwd_back=$(pwd)
->>>>>>> 17cd67652441dd88c5ac359ecc6b6d9a65b6b2a1
 
 function gtest_build() {
     mkdir ./googletest/build
     cd ./googletest/build && cmake ..
     make -j$(grep -c processor /proc/cpuinfo)
-<<<<<<< HEAD
     cd $root_path
-=======
-    cd $pwd_back
->>>>>>> 17cd67652441dd88c5ac359ecc6b6d9a65b6b2a1
 }
 
 function ffmpeg_build() {
     cd ./FFmpeg && ./configure --prefix=./build --enable-gpl --enable-static --disable-shared --enable-pthreads --enable-debug 
     make -j$(grep -c processor /proc/cpuinfo)
 	make install
-<<<<<<< HEAD
     cd $root_path
-=======
-    cd $pwd_back
->>>>>>> 17cd67652441dd88c5ac359ecc6b6d9a65b6b2a1
 }
 
 gtest_build
